816443e1cbe00996bb67ff98c311ffa43ebfb27b
justryme <stwright@oakland.edu>
2015-09-30 18:27:19 -0400

    Added UTSServerCompare windows service project.

---
+++ UTSSeverCompare/Groups/GetGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1.Groups
+{
+    class GetGroup
+    {
+        public void DatabaseConnect()
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server=[server_name];Database=[database_name];Trusted_Connection=true";
+                // using the code here...
+            }
+        }
+    }
+}

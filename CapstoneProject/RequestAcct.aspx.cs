11741d29bf0a80a59a761ee75a385e4e082bfcf4
dohrte <ecdohert@oakland.edu>
2015-10-24 18:51:45 -0400

    Added Emailer, added email functioality to req form

--- CapstoneProject/RequestAcct.aspx.cs
+++ CapstoneProject/RequestAcct.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,7 +12,46 @@ namespace CapstoneProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+          if (IsPostBack)
+          {
+            requestForm.Visible = false;
+            messagePanel.Visible = true;
+          }
+          else
+          {
+            messagePanel.Visible = false;
+            requestForm.Visible = true;
+          }
+        }
 
+        protected void requestSubmit_Click(object sender, EventArgs e)
+        {
+          Emailer emailer = new Emailer();
+          //gather form details into email body 
+          string eSubj = "Access Request from " + fNameBox.Text + " " + lNamebox.Text;
+          
+          //gather the check boxes that are checked
+          var checked_texts = this.Controls.OfType<CheckBox>()
+                                        .Where(c => c.Checked)
+                                        .Select(c => c.Text);
+
+          StringBuilder eContent = new StringBuilder();
+          eContent.AppendLine(string.Format("{0} {1} is requesting access to the web application.", fNameBox.Text, lNamebox.Text));
+          eContent.AppendLine(string.Format("Users email: {0}", emailBox.Text));
+          eContent.AppendLine(string.Format("Supervisor: {0}", supervisorBox.Text));
+          eContent.AppendLine();
+          eContent.AppendLine("The type of access requested:");
+
+          foreach (string acctType in checked_texts)
+          {
+            eContent.AppendLine(acctType);
+          }
+
+          eContent.AppendLine();
+          eContent.AppendLine(string.Format("Request reason: {0}", reasonBox.Text));
+
+
+          emailer.SendAcctRequest(eSubj, eContent.ToString());
         }
     }
 }
